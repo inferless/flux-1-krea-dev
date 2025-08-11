6f318b32347af199c31749a1f7a797c84cd659c1
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-08-11 09:16:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import torch
-from diffusers import FluxKontextPipeline
-from diffusers.utils import load_image
+from diffusers import FluxPipeline
 from typing import Optional
 import base64, io
 from pydantic import BaseModel, Field
@@ -10,33 +9,31 @@ os.environ["HF_HUB_ENABLE_HF_TRANSFER"]='1'
 
 @inferless.request
 class RequestObjects(BaseModel):
-    prompt: str = Field(default="Add a hat to the cat")
-    image_url: Optional[str] = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/cat.png"
-    guidance_scale: Optional[float] = 2.5
-    num_inference_steps: Optional[int] = 30
+    prompt: str = Field(default="A frog holding a sign that says hello world")
+    guidance_scale: Optional[float] = 4.5
+    height: Optional[int] = 1024
+    width: Optional[int] = 1024
 
 @inferless.response
 class ResponseObjects(BaseModel):
-    edited_image_base64: str = Field(default="Test Output")
+    image_base64: str = Field(default="Test Output")
 
 class InferlessPythonModel:
     def initialize(self):
-        model_id = "black-forest-labs/FLUX.1-Kontext-dev"
-        self.pipe = FluxKontextPipeline.from_pretrained(model_id,torch_dtype=torch.bfloat16).to("cuda")
+        self.pipe = pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-Krea-dev", torch_dtype=torch.bfloat16).to("cuda")
 
     def infer(self, inputs: RequestObjects) -> ResponseObjects:
-        image = load_image(inputs.image_url)
-        out = self.pipe(
-            image=image,
-            prompt=inputs.prompt,
-            guidance_scale=inputs.guidance_scale,
-            num_inference_steps=inputs.num_inference_steps,
+        image = self.pipe(
+                    inputs.prompt,
+                    height=inputs.height,
+                    width=inputs.width,
+                    guidance_scale=inputs.guidance_scale,
         ).images[0]
-
+        
         buf = io.BytesIO()
-        out.save(buf, format="PNG")
+        image.save(buf, format="PNG")
         encoded = base64.b64encode(buf.getvalue()).decode()
-        return ResponseObjects(edited_image_base64=encoded)
+        return ResponseObjects(image_base64=encoded)
 
     def finalize(self):
         self.pipe = None
